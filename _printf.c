29640eec883be051b31185825d1b8b5b8d72663d
Glory Ochako <kawiraochako@gmail.com>
2023-08-21 13:59:01 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -3,22 +3,19 @@
 int _printf(const char *format, ...)
 {
 	int print_chars = 0;
-
 	va_list args_of_list;
 
 	if (*format == NULL)
 	{
 		return (-1);
 	}
-
 	va_start(args_of_list, format);
-
-	while (*format){
-
-	if (*format != '%') {
+	while (*format)
+	{
+	if (*format != '%') 
+	{
 	write(1, format, 1);
 	print_chars++;
-
 	}
 
 	else 
@@ -29,21 +26,42 @@ int _printf(const char *format, ...)
 	break;
 	
 
-	if (*format == '%') {
+	if (*format == '%') 
+	{
 	write(1, format, 1);
 	print_chars++;
 	}
 
-	else if (*format == 'c') {
+	else if (*format == 'c') 
+	{
 		char c = va_arg(args_of_list, int);
 	write(1, &c, 1);
 	print_chars++;
 	}
 
-	else if (*format == 's'){
-		
+	else if (*format == 's') 
+	{
+		char *str = va_arg(args_of_list, char*);
+		int length = 0;
+
+		while(str[length] != '\0')
+			length++;
+		write(1, str,length);
+		print_chars += length;
 	}
-		
 	}
+	format++;
+	}
+
+	va_end(args_of_list);
+	return (print_chars);
 }
 
+int main()
+{
+	_printf("Today is\n");
+	_printf("%c\n", 'a');
+	_printf("%s\n", "Monday evening");
+	_printf("%%\n");
+	return (0);
+}
