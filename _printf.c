0956babaeb445cc95bbc2ab54491db4b0ad19d03
Glory Ochako <kawiraochako@gmail.com>
2023-08-21 14:10:37 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -5,10 +5,9 @@ int _printf(const char *format, ...)
 	int print_chars = 0;
 	va_list args_of_list;
 
-	if (*format == NULL)
-	{
+	if (format == NULL)	
 		return (-1);
-	}
+	
 	va_start(args_of_list, format);
 	while (*format)
 	{
