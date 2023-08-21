58b39e6f41e43c9c01c388bfe1511bb236a7f4e9
nivlek-nivlek <nivleknivlek111@gmail.com>
2023-08-21 23:17:00 +0300

    hot fix

--- _printf.c
+++ _printf.c
@@ -6,11 +6,44 @@ int _printf(const char *format, ...)
 
 	va_list args_of_list;
 
-	if (format == NULL)
+	if (*format == NULL)
 	{
 		return (-1);
 	}
 
 	va_start(args_of_list, format);
 
-	while (*format)
+	while (*format){
+
+	if (*format != '%') {
+	write(1, format, 1);
+	print_chars++;
+
+	}
+
+	else 
+	{
+		format++;
+
+	if (*format == '\0')
+	break;
+	
+
+	if (*format == '%') {
+	write(1, format, 1);
+	print_chars++;
+	}
+
+	else if (*format == 'c') {
+		char c = va_arg(args_of_list, int);
+	write(1, &c, 1);
+	print_chars++;
+	}
+
+	else if (*format == 's'){
+		
+	}
+		
+	}
+}
+
