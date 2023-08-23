31cec6b6b2ca7933912adb9217cb98194449626f
nivlek-nivlek <nivleknivlek111@gmail.com>
2023-08-23 22:46:02 +0300

    New Changes

--- printf1.c
+++ printf1.c
@@ -2,8 +2,34 @@
 
 int _printf(const char *format, ...)
 {
-    va_list
-    va_start
+    va_list args;
+
+    va_start(args, format);
+
+    int count = 0;
+    while (*format != "\0"){
+    
+    if (*format == '%'){
+        format ++;
+
+        if(*format == 'd' || *format == 'i')
+        { int num = va_arg(args, int);
+        
+
+
+
+
+
+
+
+        }
+
+    
+    
+     
+
+
     va_arg
+    }
     va_end
 }
